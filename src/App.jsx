a501c306809b0de157f5d6fbe9e7547c8432be71
Santoosh9 <santoshgajurel17@gmail.com>
2024-04-04 07:42:28 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello santosh!</h1>
+    </>
+  );
+};
+
+export default App;
